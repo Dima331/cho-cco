d5b0cc1a1ff7e31083723ec717e8e95173e36fed
Дмитрий Барков <C:\Users\Barkov_D\AppData\Roaming\The Bat!>
2019-06-17 16:36:42 +0300

    add team

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,20 +1,10 @@
 ;(function () {
-    // $('.reviews-list').slick({
-    //      appendArrows: false,
-    //      dots: true,
-    //      appendDots: $('.reviews-list'),
-    //      dotsClass: 'reviews-navigation',
-    //     // autoplay: true
-    // });
-
-
-    $('.reviews-list').slick({ 
+     $('.reviews-list').slick({ 
         slidesToShow: 1,  // по одному слайдеру 
         slidesToScroll: 1, // по одному менять
         arrows:false, 
         asNavFor: '.reviews-navigation' // указываем что навигация для слайдера будет отдельно (указываем класс куда вешаем навигацию)
     });
-
 
     $('.reviews-navigation').slick({ // настройка навигации
         slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
